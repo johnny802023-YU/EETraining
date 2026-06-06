ebe9fc8e74e7d631a97756c241de1f0911026822
johnny802023-YU <289173498+johnny802023-YU@users.noreply.github.com>
2026-06-06 08:20:24 +0800

    Move team progress import to admin page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import DebugPage from "./pages/DebugPage.jsx";
 import AutomotivePage from "./pages/AutomotivePage.jsx";
 import QuizPage from "./pages/QuizPage.jsx";
 import RecordsPage from "./pages/RecordsPage.jsx";
+import TeamProgressAdminPage from "./pages/TeamProgressAdminPage.jsx";
 import { useLearningProgress } from "./hooks/useLearningProgress.js";
 import { useTrainingRecords } from "./hooks/useTrainingRecords.js";
 
@@ -28,6 +29,7 @@ export default function App() {
         <Route path="automotive" element={<AutomotivePage />} />
         <Route path="quiz" element={<QuizPage />} />
         <Route path="records" element={<RecordsPage />} />
+        <Route path="admin/team-progress" element={<TeamProgressAdminPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
